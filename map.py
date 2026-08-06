f98ca73db9509b8fb8486b414c7f327a9b3009f6
monkmode3600rounds-crypto <monkmode3600rounds@gmail.com>
2026-08-06 12:11:53 +0800

    Updated map.py with stations

--- map.py
+++ map.py
@@ -2,15 +2,25 @@ import folium
 
 # 1. 屯門 8 大商場的真實經緯度座標與專屬顏色
 locations = [
-    {"id": 1, "name": "V city", "coords": [22.3953, 113.9742], "color": "#E63946"},
-    {"id": 2, "name": "屯門時代廣場北翼 Tuen Mun Trend Plaza North Wing", "coords": [22.3929, 113.9758], "color": "#2A9D8F"},
+    {"id": 1, "name": "V City", "coords": [22.3953572, 113.97403], "color": "#E63946"},
+    {"id": 2, "name": "屯門時代廣場北翼 Tuen Mun Trend Plaza North Wing", "coords": [22.39315, 113.97574], "color": "#2A9D8F"},
     {"id": 3, "name": "屯門時代廣場南翼 Tuen Mun Trend Plaza South Wing", "coords": [22.3922, 113.9757], "color": "#F4A261"},
-    {"id": 4, "name": "K point 錦薈坊", "coords": [22.3940, 113.9760], "color": "#9C27B0"},
-    {"id": 5, "name": "屯門市廣場 二期 TmT Plaza phase 2", "coords": [22.3934, 113.9768], "color": "#E91E63"},
-    {"id": 6, "name": "屯門市廣場 一期 TmT Plaza phase 1", "coords": [22.3918, 113.9769], "color": "#D32F2F"},
-    {"id": 7, "name": "WALDORF SHOPPING CENTRE (華都商場)", "coords": [22.3907, 113.9774], "color": "#F9C74F"},
-    {"id": 8, "name": "New Town Commercial Arcade 新都", "coords": [22.3900, 113.9778], "color": "#277DA1"}
+    {"id": 4, "name": "錦薈坊 K point", "coords": [22.39384, 113.9755], "color": "#9C27B0"},
+    {"id": 5, "name": "屯門市廣場 二期 TmT Plaza phase 2", "coords": [22.39407, 113.97626], "color": "#E91E63"},
+    {"id": 6, "name": "屯門市廣場 一期 TmT Plaza phase 1", "coords": [22.39286, 113.97719], "color": "#D32F2F"},
+    {"id": 7, "name": "華都商場/華都大道 WALDORF SHOPPING CENTRE/ Waldorf Avenue ", "coords": [22.39184,113.97805], "color": "#F9C74F"},
+    {"id": 8, "name": "新都 New Town Commercial Arcade ", "coords": [22.39087, 113.9783], "color": "#277DA1"}
 ]
+
+stations = [
+    {"name": "Tuen Mun Station (MTR)", "coords": [22.39494, 113.97303], "type": "mtr"},
+    {"name": "Tuen Mun Station (Light Rail)", "coords": [22.3938, 113.97326], "type": "lr"},
+    {"name": "Ho Tin Station (Light Rail)", "coords": [22.39740, 113.97313], "type": "lr"},
+    {"name": "Pui To Station (Light Rail)", "coords": [22.39461, 113.9768], "type": "lr"},
+    {"name": "Town Centre Station (Light Rail)", "coords": [22.39141, 113.97498], "type": "lr"},
+    
+]
+
 
 # 2. 以屯門市中心為原點建立真實地圖
 m = folium.Map(location=[22.3925, 113.9762], zoom_start=17, tiles="OpenStreetMap")
